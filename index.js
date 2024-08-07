bf40fcea34ab8e998333ca9314be9ae59714370b
mirzatillo <ibragimovmirzatillo27@gamil.com>
2024-08-07 11:00:22 +0500

    vazifa

---
+++ index.js
@@ -0,0 +1,193 @@
+// 1
+// let arr=[1, 2, 3, 4, 5, 9, 11]
+
+// function test(arg) {
+//     let count=0;
+//     for (const element of arg) {
+//         if (element % 2 == 1) {
+//            count++ 
+//         }
+//     }
+//     return count
+// }
+
+// 2
+// let str="salom1"
+
+// function test(arg) {
+//     let res=[];
+//     for (const element of arg) {
+//         if (element > 0) {
+//             res.push(element)
+//         }
+//     }
+//     return res
+// }
+// let res=test(arg);
+// console.log(res);
+
+// 3
+// let res="salom bober cls"
+// function test(arg) {
+//     let res=arg.split(' ')
+//     let coutn=0;
+//    for (let i = 0; i < res; i++) {
+//        coutn++
+//    }
+//     return count
+// }
+
+// 4
+// let arr=[1, 2, 3]
+
+// function test(arg) {
+//     let min=0;
+//     for (const element of arg) {
+//         if (min < element) {
+//             min = min
+//         }
+//     }
+//     return min
+// }
+
+// 6
+// let arr=[1, 2, 3, 4, 5]
+
+// function test(arg) {
+//     let res=1;
+//     for (let i = 0; i < arr; i++) {
+//         res*=i        
+//     }
+//     return res
+// }
+
+
+// 7
+// let arr =[1, 3, 5, 6, 2, 3, 9, 10]
+// function test(arg) {
+//     return arg.sort(function(a,b) {
+//         return a - b;
+//     })
+// }
+// let res = test(arr);
+// console.log(res);
+
+
+// 9
+// let str="salom bober"
+
+// function test(arg) {
+//     for (const element of arg) {
+//         return str.toUpperCase
+//     }
+// }
+
+// 10
+// let arr=[1, 2, 3, 4, 5, 6, 7, 11]
+
+// function test(arg) {
+//     let res=[];
+//     for (const element of arg) {
+//         if (element % 2 ==1) {
+//             res.push(element)
+//         }
+//     }
+//     return res
+// }
+// let res=test(arg)
+// console.log(res);
+
+
+// 11
+// let arr=["salom", "bober", "shopdi"]
+
+// function test(arg) {
+//     let max=0;
+//     let res=[];
+//     for (const element of arg) {
+//         if (max < element) {
+//             res.push(max)
+//         }
+//     }
+//     return res
+// }
+// let res=test(arg)
+// console.log(res);
+
+// 12
+// let arr=[1, 2, 3, 4]
+
+// function test(arg) {
+//     let sum=0;
+//     for (let i = 0; i < arg.length; i++) {
+//        sum +=arg[i]    
+//     }
+//     return sum
+// }
+
+
+// 13
+// let arr=[-1, 2, -3,  4, -5]
+
+// function test(arg) {
+//     let m =0;
+//     for (const element of arg) {
+//         if (element > 0) {
+//           res.push(element)
+//         }
+//     }
+//     return res
+// }
+
+// 14
+// let arr=["salom", "bober", "shodi"]
+
+// function test(arg) {
+//     for (let i = 0; i < arr.length; i++) {
+//         const element = arr[i];
+
+//     }
+//     return  arr[i]
+// }
+
+// 15
+// let arr = [1, 2, 3, 4, 5, 6]
+
+// function test(arg) {
+//     let count=0;
+//     for (let i = 0; i < arr; i++) {
+//          count = arr ** 2
+//     }
+//     return count
+// }
+
+// 17
+
+// let arr =[1, 2, 3, 4]
+// function test(arg) {
+//     return arg.sort(function(a,b) {
+//         return a - b;
+//     })
+// }
+// let res = test(arr);
+// console.log(res);
+
+// 20
+
+// let str="salom bober"
+
+// function test(arg) {
+//     for (const element of arg) {
+//         return str.toLowerCase
+//     }
+// }
+
+// 21
+// let str="salom bober"
+
+// function test(arg) {
+//     for (const element of arg) {
+        
+//     }
+// }
+
